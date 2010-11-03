890e2b55adf795e6fe237b05ef0561cf8e46c55b
Endre Oma <eoma@kvarteret.no>
2010-11-03 21:26:26 +0100

    Added simple setup script called setup.sh
    This take care of initialising submodules,
    create directories, etc.

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# This script will setup the required steps to using this application
+git submodule init
+git submodule update
+
+mkdir cache/ log/
+php symfony project:permissions
+php symfony configure:database "sqlite:db.db"
