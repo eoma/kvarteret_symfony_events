c99d102b656f13dc42dcdfcf4bb4b2b12aac2ba6
Endre Oma <eoma@kvarteret.no>
2010-11-04 17:28:41 +0100

    Add initial api module.

---
+++ apps/frontend/modules/api/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * api actions.
+ *
+ * @package    kvarteret_events
+ * @subpackage api
+ * @author     Endre Oma
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class apiActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
