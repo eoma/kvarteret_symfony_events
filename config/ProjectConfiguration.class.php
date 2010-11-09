cff1fb8baa9c3958bf5c03d26aa6f95ebe2465ef
Endre Oma <eoma@kvarteret.no>
2010-11-09 20:37:42 +0100

    Enable sfCKEditorPlugin

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -3,11 +3,17 @@
 require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
+// Necessary as symfonys autoload methods don't 
+if (!defined('HTMLPURIFIER_PREFIX')) {
+    define('HTMLPURIFIER_PREFIX', realpath(dirname(__FILE__) . '/../lib/vendor/htmlpurifier/library'));
+}
+
 class ProjectConfiguration extends sfProjectConfiguration
 {
   public function setup()
   {
     $this->enablePlugins('sfDoctrinePlugin');
     $this->enablePlugins('sfDoctrineNestedSetPlugin');
+    $this->enablePlugins('sfCKEditorPlugin');
   }
 }
