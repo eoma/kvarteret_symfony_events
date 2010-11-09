bb090864429df7c28fee8dc138f725018e393dba
Endre Oma <eoma@kvarteret.no>
2010-11-09 17:48:24 +0100

    Create symlink to sfDoctrinePlugin web res. when running setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,22 @@ git submodule init
 git submodule update
 
 mkdir cache/ log/
+
+#  We'll be using the sfDoctrinePlugin's web resources
+if [ -d lib/vendor/symfony/lib/plugins/sfDoctrinePlugin ]; then
+  if [ ! -h web/sfDoctrinePlugin ]; then
+    cd web/;
+
+    ln -s ../lib/vendor/symfony/lib/plugins/sfDoctrinePlugin sfDoctrinePlugin;
+
+    cd ..;
+  else
+    echo "The symlink web/sfDoctrinePlugin already exists";
+  fi;
+else
+  echo "!!! Symfony was not checked out successfully !!!";
+fi;
+
 php symfony project:permissions
 php symfony configure:database "sqlite:db.db"
 php symfony doctrine:build --all-classes --sql
