5127b2115b626ce1afbec4f93e7ea22ac072a7ee
Endre Oma <eoma@kvarteret.no>
2010-11-09 20:40:48 +0100

    Standard layout for backend; borrowed from Symfonys jobeet

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -1,14 +1,49 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-    <?php include_http_metas() ?>
-    <?php include_metas() ?>
-    <?php include_title() ?>
+    <title>Event Admin Interface</title>
     <link rel="shortcut icon" href="/favicon.ico" />
-    <?php include_stylesheets() ?>
+    <?php use_stylesheet('admin.css') ?>
     <?php include_javascripts() ?>
+    <?php include_stylesheets() ?>
   </head>
   <body>
-    <?php echo $sf_content ?>
+    <div id="container">
+      <div id="header">
+        <h1>
+          <a href="<?php echo url_for('homepage') ?>">
+            <img src="/images/logo.jpg" alt="Jobeet Job Board" />
+          </a>
+        </h1>
+      </div>
+ 
+      <div id="menu">
+        <ul>
+          <li>
+            <?php echo link_to('Events', 'event') ?>
+          </li>
+          <li>
+            <?php echo link_to('Categories', 'category') ?>
+          </li>
+          <li>
+            <?php echo link_to('Arrangers', 'arranger') ?>
+          </li>
+          <li>
+            <?php echo link_to('Locations', 'location') ?>
+          </li>
+        </ul>
+      </div>
+ 
+      <div id="content">
+        <?php echo $sf_content ?>
+      </div>
+ 
+      <div id="footer">
+        <img src="/images/jobeet-mini.png" />
+        powered by <a href="http://www.symfony-project.org/">
+        <img src="/images/symfony.gif" alt="symfony framework" /></a>
+      </div>
+    </div>
   </body>
 </html>
