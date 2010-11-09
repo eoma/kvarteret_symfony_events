2f422d244768fdc4f6c0ab2fb2257ff7658bdb3e
Endre Oma <eoma@kvarteret.no>
2010-11-09 19:23:45 +0100

    Add backend module for managing events

---
+++ apps/backend/modules/event/lib/eventGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * event module configuration.
+ *
+ * @package    kvarteret_events
+ * @subpackage event
+ * @author     Endre Oma
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class eventGeneratorConfiguration extends BaseEventGeneratorConfiguration
+{
+}
