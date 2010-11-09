3bedf644a2eedd34e2396ce39dea0eec6039f911
Endre Oma <eoma@kvarteret.no>
2010-11-09 16:43:31 +0100

    Add backend module for managing arrangers

---
+++ apps/backend/modules/arranger/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/arrangerGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/arrangerGeneratorHelper.class.php';
+
+/**
+ * arranger actions.
+ *
+ * @package    kvarteret_events
+ * @subpackage arranger
+ * @author     Endre Oma
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class arrangerActions extends autoArrangerActions
+{
+}
