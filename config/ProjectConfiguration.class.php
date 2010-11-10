24e99d5f2cdfbc5db43d373ca6881bbe057b2860
Endre Oma <eoma@kvarteret.no>
2010-11-10 12:54:21 +0100

    Update doc. of why we must define HTMLPURIFIER_PREFIX

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -3,7 +3,7 @@
 require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
-// Necessary as symfonys autoload methods don't 
+// Necessary as symfonys autoload methods won't let specify a bootstrap script for HTMLPurifier
 if (!defined('HTMLPURIFIER_PREFIX')) {
     define('HTMLPURIFIER_PREFIX', realpath(dirname(__FILE__) . '/../lib/vendor/htmlpurifier/library'));
 }
