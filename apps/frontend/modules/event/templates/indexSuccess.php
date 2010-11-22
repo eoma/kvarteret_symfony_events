da09f21c7169c50d7cdf958ce1287e57701998cd
Endre Oma <eoma@kvarteret.no>
2010-11-22 10:42:55 +0100

    Use proper link methods when referencing other modules
    Fixed also an issue where if you put a '/' at the start of the
    url, symfony won't do it's routing "magic".

--- apps/frontend/modules/event/templates/indexSuccess.php
+++ apps/frontend/modules/event/templates/indexSuccess.php
@@ -16,19 +16,19 @@
   <tbody>
     <?php foreach ($events as $event): ?>
     <tr>
-      <td><a href="<?php echo url_for('event/show?id='.$event['id']) ?>"><?php echo $event['title'] ?></a></td>
+      <td><?php echo link_to($event['title'], 'event/show?id=' . $event['id']) ?></td>
       <td><?php echo $event->getRaw('leadParagraph') ?></td>
       <td><?php 
                 if ( ! $event['location_id'] ) 
                   echo $event['customLocation'];
                 else 
-                  echo '<a href="' . url_for('location/show?id=' . $event['location_id']) . '">' . $event['recurringLocation']['name'] . '</a>';
+                  echo link_to($event['recurringLocation']['name'], 'location/show?id=' . $event['location_id']);
           ?></td>
       <td><?php echo $event['linkout'] ?></td>
       <td><?php echo $event['startDate'] . ' ' .$event['startTime']  ?></td>
       <td><?php echo $event['endDate'] . ' ' . $event['endTime'] ?></td>
-      <td><a href="<?php echo url_for('category/show?id=' . $event['category_id']) ?>"><?php echo $event['category']['name'] ?></a></td>
-      <td><a href="<?php echo url_for('arranger/show?id=' . $event['arranger_id']) ?>"><?php echo $event['arranger']['name'] ?></a></td>
+      <td><?php echo link_to($event['category']['name'], 'category/show?id=' . $event['category_id']) ?></td>
+      <td><?php echo link_to($event['arranger']['name'], 'arranger/show?id=' . $event['arranger_id']) ?></td>
     </tr>
     <?php endforeach; ?>
   </tbody>
