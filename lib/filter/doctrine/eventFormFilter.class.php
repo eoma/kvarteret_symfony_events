2f4c0d4048690bb98fa6329e735ba0052e49ccd0
Endre Oma <eoma@kvarteret.no>
2010-11-23 16:38:03 +0100

    Let arrangers to only filter on events related to the arranger

--- lib/filter/doctrine/eventFormFilter.class.php
+++ lib/filter/doctrine/eventFormFilter.class.php
@@ -12,5 +12,20 @@ class eventFormFilter extends BaseeventFormFilter
 {
   public function configure()
   {
+    if (!($this->getOption('currentUser')) instanceof sfGuardSecurityUser) {
+      throw new InvalidArgumentException("You must pass a user object as an option to this form!");
+    }
+
+    if ( ! $this->getOption('currentUser')->hasGroup('admin') ) {
+      // Widget arranger_is of type sfWidgetFormDoctrineChoice, which supports queries.
+      // If the user is not an admin, we make sure to only use
+      // the arrangers the user is limited to.
+      $user = $this->getOption('currentUser')->getGuardUser();
+
+      $this->widgetSchema['arranger_id']->setOption('query',
+        Doctrine_Core::getTable('arranger')->createQuery('a')->select('a.*')->leftJoin('a.users u')->where('u.user_id = ?', $user->getId())
+      );
+    }
+
   }
 }
