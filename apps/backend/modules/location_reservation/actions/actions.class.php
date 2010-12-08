60588cba5200e0ae8456d16968a005845e8dec50
Endre Oma <eoma@kvarteret.no>
2010-12-08 11:47:02 +0100

    Add backend module location_reservation

---
+++ apps/backend/modules/location_reservation/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/location_reservationGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/location_reservationGeneratorHelper.class.php';
+
+/**
+ * location_reservation actions.
+ *
+ * @package    kvarteret_events
+ * @subpackage location_reservation
+ * @author     Endre Oma
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class location_reservationActions extends autoLocation_reservationActions
+{
+}
---
+++ apps/backend/modules/location_reservation/lib/location_reservationGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * location_reservation module configuration.
+ *
+ * @package    kvarteret_events
+ * @subpackage location_reservation
+ * @author     Endre Oma
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class location_reservationGeneratorConfiguration extends BaseLocation_reservationGeneratorConfiguration
+{
+}
