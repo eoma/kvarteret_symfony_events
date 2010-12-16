c12e211428237f0536044c8534c779273a2810a2
Endre Oma <eoma@kvarteret.no>
2010-12-16 13:20:12 +0100

    Replace custom HTMLPurifier code by myHTMLPurifuier methods in event model

--- lib/model/doctrine/event.class.php
+++ lib/model/doctrine/event.class.php
@@ -13,6 +13,8 @@
 class event extends Baseevent
 {
 
+  protected $purifier = null;
+
   /**
    * Returns recurring location if location_id is set, 
    * if not it will return custom location
@@ -27,60 +29,25 @@ class event extends Baseevent
   }
 
   /**
+   * Will check if myHTMLPurifier has been initiated, if not, initiate it.
+   */
+  protected function myHTMLPurifierInstance() {
+    if (empty($this->purifier)) $this->purifier = new myHTMLPurifier();
+  }
+
+  /**
    * setDescription will escape and remove illegal tags and script in description.
    * We'll only allow certain tags, and certain attributes on tags
    */
   public function setDescription ($value) {
-    $purifier = new myHTMLPurifier();
+    $this->myHTMLPurifierInstance();
 
-    // purifier configuration:
-    // documentation: http://stackoverflow.com/questions/1320524/how-to-allow-certain-html-tags-in-a-form-field-in-symfony-1-2/1321794#1321794
-    $purifier_config = $purifier->createConfig();
-
-    $purifier_config->set('HTML.DefinitionID', 'eventDescription');
-    $purifier_config->set('HTML.DefinitionRev', 1);
-
-    // clean empty tags
-    $purifier_config->set('AutoFormat.RemoveEmpty', true);
-    //$purifier_config->set('AutoFormat.RemoveEmpty.RemoveNbsp', true);
-    //$purifier_config->set('AutoFormat.RemoveEmpty.RemoveNbsp.Exceptions', array());
-
-    // these are allowed html tags, means white list
-    $purifier_config->set('HTML.AllowedElements', 'a,strong,em,p,span,img,li,ul,ol,blockquote');
-
-    // these are allowed html attributes, coool!
-    $purifier_config->set('HTML.AllowedAttributes', 'a.href,a.title,span.style,span.class,span.id,p.style,img.src,img.style,img.alt,img.title,img.width,img.height');
-
-    // now clean your data
-    $clean_html = $purifier->purify($value, $purifier_config);
-
-    $this->_set('description', $clean_html);
+    $this->_set('description', trim($this->purifier->commonHtml($value)));
   }
 
   public function setLeadParagraph ($value) {
-    $purifier = new myHTMLPurifier();
+    $this->myHTMLPurifierInstance();
 
-    // purifier configuration:
-    // documentation: http://stackoverflow.com/questions/1320524/how-to-allow-certain-html-tags-in-a-form-field-in-symfony-1-2/1321794#1321794
-    $purifier_config = $purifier->createConfig();
-
-    $purifier_config->set('HTML.DefinitionID', 'eventLeadParagraph');
-    $purifier_config->set('HTML.DefinitionRev', 1);
-
-    // clean empty tags
-    $purifier_config->set('AutoFormat.RemoveEmpty', true);
-    $purifier_config->set('AutoFormat.RemoveEmpty.RemoveNbsp', true);
-    //$purifier_config->set('AutoFormat.RemoveEmpty.RemoveNbsp.Exceptions', array());
-
-    // these are allowed html tags, means white list
-    $purifier_config->set('HTML.AllowedElements', 'p');
-
-    // these are allowed html attributes, coool!
-    //$purifier_config->set('HTML.AllowedAttributes', '');
-
-    // now clean your data
-    $clean_html = $purifier->purify($value, $purifier_config);
-
-    $this->_set('leadParagraph', $clean_html);
+    $this->_set('leadParagraph', trim($this->purifier->basicHtml($value)));
   }
 }
