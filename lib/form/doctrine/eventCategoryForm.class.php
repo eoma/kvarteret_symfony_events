5c8556875c7e29b718f43f0aec6c367a6a1466ae
Endre Oma <eoma@kvarteret.no>
2010-12-29 12:39:40 +0100

    Add eventCategory classes for form, filter and model

---
+++ lib/form/doctrine/eventCategoryForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * eventCategory form.
+ *
+ * @package    kvarteret_events
+ * @subpackage form
+ * @author     Endre Oma
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class eventCategoryForm extends BaseeventCategoryForm
+{
+  public function configure()
+  {
+  }
+}
