e4af0ea0fd8c3a145d475f289d36a9b3b8112632
Endre Oma <eoma@kvarteret.no>
2010-12-29 12:25:48 +0100

    Add methods defaultJoins and defaultOrderBy to class eventTable

--- lib/model/doctrine/eventTable.class.php
+++ lib/model/doctrine/eventTable.class.php
@@ -16,4 +16,25 @@ class eventTable extends Doctrine_Table
     {
         return Doctrine_Core::getTable('event');
     }
+
+    public function defaultJoins(Doctrine_Query $q)
+    {
+        $rootAlias = $q->getRootAlias();
+
+        $q->leftJoin($rootAlias . '.recurringLocation l')
+          ->leftJoin($rootAlias . '.arranger a')
+          ->leftJoin($rootAlias . '.categories c')
+          ->leftJoin($rootAlias . '.festival f');
+
+        return $q;
+    }
+
+    public function defaultOrderBy(Doctrine_Query $q)
+    {
+        $rootAlias = $q->getRootAlias();
+
+        $q->orderBy($rootAlias . '.startDate asc, ' .
+                    $rootAlias . '.startTime asc, ' .
+                    $rootAlias . '.title asc');
+    }
 }
