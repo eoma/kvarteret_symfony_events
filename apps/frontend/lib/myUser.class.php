baedbdab6826060670844ea673b0bf4ff7f9fbd5
Endre Oma <eoma@kvarteret.no>
2011-01-01 21:58:19 +0100

    Add first request detector for web interface

--- apps/frontend/lib/myUser.class.php
+++ apps/frontend/lib/myUser.class.php
@@ -2,4 +2,15 @@
 
 class myUser extends sfBasicSecurityUser
 {
+
+  public function isFirstRequest($boolean = null)
+  {
+    if (is_null($boolean))
+    {
+      return $this->getAttribute('first_request', true);
+    }
+ 
+    $this->setAttribute('first_request', $boolean);
+  }
+
 }
