6b56dfe1079cc32c4dd67f89046d2c85eee47c45
Endre Oma <eoma@kvarteret.no>
2011-01-07 19:45:36 +0100

    Add atom output success files for upcomingEvents and filteredEvents, add partial eventList for atom

---
+++ apps/frontend/modules/api/templates/_eventList.atom.php
@@ -0,0 +1,25 @@
+<?php use_helper('Date') ?>
+<?php foreach ($events as $e): ?>
+    <entry>
+      <title>
+        <?php echo $e['title'] . "\n" ?>
+      </title>
+      <link href="<?php echo url_for('event/show?id=' . $e['id'], true) ?>" />
+      <id><?php echo sha1($e['id']) ?></id>
+      <updated><?php echo gmstrftime('%Y-%m-%dT%H:%M:%SZ', strtotime($e['updated_at'])) ?></updated>
+      <summary type="xhtml">
+        <div xmlns="http://www.w3.org/1999/xhtml">
+          <p>
+            <strong><?php echo __('Location') ?>:</strong> <?php if ($e['location_id'] > 0) { echo $e['recurringLocation']['name']; } else { echo $e['customLocation']; } ?><br />
+            <strong><?php echo __('Start') ?>:</strong> <?php echo format_datetime($e['startDate'] . ' ' . $e['startTime']) ?><br />
+            <strong><?php echo __('Arranger') ?>:</strong> <?php echo $e['arranger']['name'] ?><br />
+            <strong><?php echo __('Category') ?>:</strong> <ul><?php foreach($e['categories'] as $c) { echo '<li>' . $c['name'] . '</li>'; } ?></ul>
+          </p>
+ 
+          <p>
+            <?php echo $e->getRaw('leadParagraph') ?>
+          </p>
+        </div>
+      </summary>
+    </entry>
+<?php endforeach ?>
