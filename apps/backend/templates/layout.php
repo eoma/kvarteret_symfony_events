41ccd4feb2c53232d145d147cf4f9f0bbf26b110
Endre Oma <eoma@kvarteret.no>
2011-04-04 16:09:09 +0200

    Update layout

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -9,65 +9,64 @@
     <?php include_javascripts() ?>
   </head>
   <body>
-    <div id="container">
-      <div id="header">
-        <div id="menu">
-          <ul>
-            <?php if ($sf_user->isAuthenticated()): ?>
-            <!-- only show the meny when authenticated -->
-            <li>
-              <?php echo link_to('Events', 'dak_event_admin') ?>
-            </li>
-            <li>
-              <?php echo link_to('Festivals', 'dak_festival_admin') ?>
-            </li>
-            <li>
-              <?php echo link_to('Categories', 'dak_category_admin') ?>
-            </li>
-            <li>
-              <?php echo link_to('Arrangers', 'dak_arranger_admin') ?>
-            </li>
-            <li>
-              <?php echo link_to('Locations', 'dak_location_admin') ?>
-            </li>
-            <li>
-              <?php echo link_to('Location reservations', 'dak_location_reservation_admin') ?>
-            </li>
-            <li>
-              <?php echo link_to('Pictures', 'dak_picture_admin') ?>
-            </li>
-            <li>
-            <?php if ($sf_user->isSuperAdmin()): ?>
-            <!-- If the user is a super admin we show the group and permission pages -->
-            <li>
-              <?php echo link_to('Users', 'sf_guard_user') ?>
-            </li>
-            <li>
-              <?php echo link_to('Groups', 'sf_guard_group') ?>
-            </li>
-            <li>
-              <?php echo link_to('Permissions', 'sf_guard_permission') ?>
-            </li>
-            <?php endif ?>
-            <li>
-              <?php echo link_to('Logout', 'sf_guard_signout') ?>
-            </li>
-            <?php endif ?> 
-	    <li>
-              <a href="<?php echo public_path('', true) ?>">Frontend</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div id="content">
-        <?php echo $sf_content ?>
-      </div>
- 
-      <div id="footer">
-        <img src="/images/jobeet-mini.png" />
-        powered by <a href="http://www.symfony-project.org/">
-        <img src="/images/symfony.gif" alt="symfony framework" /></a>
-      </div>
+    <div id="header">
+    </div>
+    <div id="menu">
+      <ul>
+        <?php if ($sf_user->isAuthenticated()): ?>
+        <!-- only show the meny when authenticated -->
+        <li>
+          <?php echo link_to('Events', '@dak_event_admin') ?>
+        </li>
+        <li>
+          <?php echo link_to('Festivals', '@dak_festival_admin') ?>
+        </li>
+        <li>
+          <?php echo link_to('Categories', '@dak_category_admin') ?>
+        </li>
+        <li>
+          <?php echo link_to('Arrangers', '@dak_arranger_admin') ?>
+        </li>
+        <li>
+          <?php echo link_to('Locations', '@dak_location_admin') ?>
+        </li>
+        <li>
+          <?php echo link_to('Pictures', '@dak_picture_admin') ?>
+        </li>
+        <li>
+          <?php echo link_to('Profile', '@sf_guard_user_edit?id=' . $sf_user->getGuardUser()->getId()) ?>
+        </li>
+        <!--<li>
+          <?php //echo link_to('Location reservations', 'location_reservation') ?>
+        </li>-->
+        <?php if ($sf_user->hasGroup('admin')): ?>
+        <!-- If the user is a super admin we show the group and permission pages -->
+        <li>
+          <?php echo link_to('Users', '@sf_guard_user') ?>
+        </li>
+        <li>
+          <?php echo link_to('Groups', '@sf_guard_group') ?>
+        </li>
+        <li>
+          <?php echo link_to('Permissions', '@sf_guard_permission') ?>
+        </li>
+        <!-- endif has group admin-->
+        <?php endif ?>
+        <li>
+           <?php echo link_to('Logout', '@sf_guard_signout') ?>
+        </li>
+        <!--  endif authenticated-->
+        <?php endif ?> 
+        <li>
+          <a href="<?php echo public_path('', true) ?>">Frontend</a>
+        </li>
+      </ul>        
+    </div>
+
+    <div id="content">
+      <?php echo $sf_content ?>
+    </div>
+    <div id="footer">
     </div>
   </body>
 </html>
--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -6,17 +6,21 @@
     <title><?php include_slot('title') ?> - <?php echo __('Event Calendar') ?></title>
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_stylesheets() ?>
+    <!-- Start include javascript -->
+    <?php include_javascripts() ?>
+    <!-- End include javascript -->
+    <?php include_slot('feeds') ?>
   </head>
   <body>
     <div id="header">
       <div id="menu">
         <ul>
-          <li><?php echo link_to(__('Main'), '@homepage_localized') ?></li>
+          <li><?php echo link_to(__('Main'), '@dak_event_index') ?></li>
           <li><?php echo link_to(__('Festivals'), '@dak_festival_index') ?></li>
           <li><?php echo link_to(__('Locations'), '@dak_location_index') ?></li>
           <li><?php echo link_to(__('Arrangers'), '@dak_arranger_index') ?></li>
           <li><?php echo link_to(__('Categories'), '@dak_category_index') ?></li>
-          <li><?php echo link_to(__('API documentation'), '@dak_api') ?></li>
+          <li><?php echo link_to(__('API documentation'), 'dak_api') ?></li>
           <li><a href="<?php echo public_path('', true) ?>backend.php">Backend</a></li>
         </ul>
       </div>
@@ -25,14 +29,11 @@
       <?php echo $sf_content ?>
     </div>
 
-    <div style="clear: both">
+    <div id="footer" style="clear: both">
       <ul>
-        <li><?php echo link_to('View in english', '@homepage_localized?sf_culture=en') ?></li>
-        <li><?php echo link_to('Vis på norsk', '@homepage_localized?sf_culture=no') ?></li>
+        <li><?php echo link_to('View in english', '@dak_event_index?sf_culture=en') ?></li>
+        <li><?php echo link_to('Vis på norsk', '@dak_event_index?sf_culture=no') ?></li>
       </ul>
     </div>
-    <!-- Start include javascript -->
-    <?php include_javascripts() ?>
-    <!-- End include javascript -->
   </body>
 </html>
