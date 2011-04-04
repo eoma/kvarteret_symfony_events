306f52935485451dc063b6afa62fb4648d2d9008
Endre Oma <eoma@kvarteret.no>
2011-04-04 17:59:46 +0200

    Split setup.sh into a setup script and a update script

--- setup.sh
+++ setup.sh
@@ -1,107 +1,14 @@
 #!/bin/sh
 
-root_dir=$PWD;
-
-# This script will setup the required steps to using this application
-git submodule sync
-git submodule init
-git submodule update
-
-mkdir cache/ log/
-
-#  We'll be using the sfDoctrinePlugin's web resources
-if [ -d lib/vendor/symfony/lib/plugins/sfDoctrinePlugin/web ]; then
-  if [ ! -h web/sfDoctrinePlugin ]; then
-    cd web/;
-
-    ln -s ../lib/vendor/symfony/lib/plugins/sfDoctrinePlugin/web sfDoctrinePlugin;
-
-    cd ..;
-  else
-    echo "The symlink web/sfDoctrinePlugin already exists";
-  fi;
-else
-  echo "!!! Symfony was not checked out successfully !!!";
-fi;
-
-# Make sure the plugins directory exist
-if [ ! -d plugins/ ]; then
-  mkdir plugins;
-fi;
-
-
-### BEGIN SETUP OF PLUGINS ###
-
-# Download and setup sfDoctrineNestedSetPlugin, remove previous version if set.
-cd plugins;
-
-if [ -d sfDoctrineNestedSetPlugin ]; then
-  rm -r sfDoctrineNestedSetPlugin;
-fi;
-
-wget http://plugins.symfony-project.org/get/sfDoctrineNestedSetPlugin/sfDoctrineNestedSetPlugin-1.0.0.tgz
-tar -xzf sfDoctrineNestedSetPlugin-1.0.0.tgz
-mv sfDoctrineNestedSetPlugin-1.0.0 sfDoctrineNestedSetPlugin
-rm sfDoctrineNestedSetPlugin-1.0.0.tgz
-
-cd $root_dir;
-
-# Download and setup ckeditor
-cd web;
-if [ ! -d js ]; then
-  mkdir js;
-fi;
-cd js;
-
-if [ -d ckeditor ]; then
-  rm -r ckeditor
-fi;
-
-wget http://download.cksource.com/CKEditor/CKEditor/CKEditor%203.4.2/ckeditor_3.4.2.tar.gz
-tar -xzf ckeditor_3.4.2.tar.gz
-rm ckeditor_3.4.2.tar.gz
-
-cd $root_dir;
-
-# Download and setup htmlpurifier
-cd lib/vendor;
-
-if [ -d htmlpurifier ]; then
-  rm -rf htmlpurifier
-fi;
-
-wget http://htmlpurifier.org/releases/htmlpurifier-4.3.0.tar.gz
-tar -xzf htmlpurifier-4.3.0.tar.gz
-mv htmlpurifier-4.3.0 htmlpurifier
-rm htmlpurifier-4.3.0.tar.gz
-
-cd $root_dir
-
-# Download and setup FullCalendar
-cd web
-mkdir -p js/fullcalendar
-mkdir -p css/fullcalendar
-
-wget http://arshaw.com/fullcalendar/downloads/fullcalendar-1.5.zip
-unzip fullcalendar-1.5.zip
-mv fullcalendar-1.5/fullcalendar/*.js js/fullcalendar
-mv fullcalendar-1.5/fullcalendar/*.css css/fullcalendar
-
-rm -r fullcalendar-1.5
-rm fullcalendar-1.5.zip
-
-cd $root_dir;
-
-### END SETUP OF PLUGINS ###
-
-php symfony project:permissions
+# Setup plugins and stuff
+source ./update.sh
 
 # We make sure we can delete the contents of the cache by setting the
 # sgid and suid bit
 chmod -R a+rws cache
 chmod -R a+rws log
 
-# Upload folter
+# Upload folder
 mkdir -p web/uploads
 chmod a+rws web/uploads
 
@@ -109,13 +16,8 @@ chmod a+rws web/uploads
 mkdir -p web/thumbs
 chmod a+rws web/thumbs
 
-# Copy dakEventsPlugin fixtures to this projects fixtures
-mkdir -p data/fixtures
-cd plugins/dakEventsPlugin/data/fixtures
-for file in *.sample; do
-  cp "$file" "${root_dir}/data/fixtures/${file/.sample}";
-done;
-cd $root_dir;
+# Generate database classes
+php symfony doctrine:build --all-classes --sql
 
 # We'll confgiure the database if the configuration
 # does not exist
@@ -123,8 +25,3 @@ cd $root_dir;
 if [ ! -e config/databases.yml ]; then
   php symfony configure:database "sqlite:db.db";
 fi;
-
-php symfony doctrine:build --all-classes --sql
-
-# Make sure our plugins' web resources are accessible
-php symfony plugin:publish-assets
---
+++ update.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+
+root_dir=$PWD;
+
+# This script will setup the required steps to using this application
+
+git submodule sync
+git submodule init
+git submodule update
+
+mkdir cache/ log/
+
+# Make sure the plugins directory exist
+if [ ! -d plugins/ ]; then
+  mkdir plugins;
+fi;
+
+### BEGIN SETUP OF PLUGINS ###
+
+# Download and setup sfDoctrineNestedSetPlugin, remove previous version if set.
+cd plugins;
+
+if [ -d sfDoctrineNestedSetPlugin ]; then
+  rm -r sfDoctrineNestedSetPlugin;
+fi;
+
+wget http://plugins.symfony-project.org/get/sfDoctrineNestedSetPlugin/sfDoctrineNestedSetPlugin-1.0.0.tgz
+tar -xzf sfDoctrineNestedSetPlugin-1.0.0.tgz
+mv sfDoctrineNestedSetPlugin-1.0.0 sfDoctrineNestedSetPlugin
+rm sfDoctrineNestedSetPlugin-1.0.0.tgz
+
+cd $root_dir;
+
+# Download and setup ckeditor
+cd web;
+if [ ! -d js ]; then
+  mkdir js;
+fi;
+cd js;
+
+if [ -d ckeditor ]; then
+  rm -r ckeditor
+fi;
+
+wget http://download.cksource.com/CKEditor/CKEditor/CKEditor%203.4.2/ckeditor_3.4.2.tar.gz
+tar -xzf ckeditor_3.4.2.tar.gz
+rm ckeditor_3.4.2.tar.gz
+
+cd $root_dir;
+
+# Download and setup htmlpurifier
+cd lib/vendor;
+
+if [ -d htmlpurifier ]; then
+  rm -rf htmlpurifier
+fi;
+
+wget http://htmlpurifier.org/releases/htmlpurifier-4.3.0.tar.gz
+tar -xzf htmlpurifier-4.3.0.tar.gz
+mv htmlpurifier-4.3.0 htmlpurifier
+rm htmlpurifier-4.3.0.tar.gz
+
+cd $root_dir
+
+# Download and setup FullCalendar
+cd web
+mkdir -p js/fullcalendar
+mkdir -p css/fullcalendar
+
+wget http://arshaw.com/fullcalendar/downloads/fullcalendar-1.5.zip
+unzip fullcalendar-1.5.zip
+mv fullcalendar-1.5/fullcalendar/*.js js/fullcalendar
+mv fullcalendar-1.5/fullcalendar/*.css css/fullcalendar
+
+rm -r fullcalendar-1.5
+rm fullcalendar-1.5.zip
+
+cd $root_dir;
+
+### END SETUP OF PLUGINS ###
+
+# Copy dakEventsPlugin fixtures to this projects fixtures
+mkdir -p data/fixtures
+cd plugins/dakEventsPlugin/data/fixtures
+for file in *.sample; do
+  cp "$file" "${root_dir}/data/fixtures/${file/.sample}";
+done;
+cd $root_dir;
+
+# Some housecleaning
+php symfony cc
+php symfony project:permissions
+
+# Make sure our plugins' web resources are accessible
+php symfony plugin:publish-assets
